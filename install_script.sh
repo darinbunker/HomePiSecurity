5ab619a4de71746b84fb4d37753e123048c78934
Darin Bunker <darin.bunker@domo.com>
2015-09-20 18:11:22 -0600

    updated install_script.sh with various fixes

--- install_script.sh
+++ install_script.sh
@@ -3,31 +3,44 @@
 # ################################################################ # 
 # Installation script for HomePiSecurity
 
+# Set variables - MySQL Password
+if [ $# -gt 0 ]
+then
+    MySQLPass=$1
+else
+    MySQLPass="TheMySQLPass1"
+fi
+
 # System Update and Upgrade
+printf "Execute apt-get update...\n"
 sudo apt-get update -q
-sudo apt-get upgrade -q
- 
+printf "Execute apt-get upgrade...\n"
+sudo apt-get upgrade -y --force-yes -qq
+
 # Install MySQL Server
+printf "Execute install of MySQL...\n"
+echo mysql-server-5.1 mysql-server/root_password password $MySQLPass | debconf-set-selections
+echo mysql-server-5.1 mysql-server/root_password_again password $MySQLPass | debconf-set-selections
 apt-get -q -y install mysql-server
-mysqladmin -u root password TheMySQLPass1
 
 # Install the Python MySQL components
-sudo apt-get install python-mysqldb -q
+printf "Execute install of Python MySQL components...\n"
+sudo apt-get -y install python-mysqldb -q
 
 # Install the Twilio interface components
-sudo apt-get install python-setuptools -q
+printf "Execute install of Twilio components...\n"
+sudo apt-get -y install python-setuptools -q
 sudo easy_install pip
 yes | sudo pip install twilio
 
 # Install Apache Web Server
+printf "Execute install of Apache Web Server...\n"
 sudo apt-get install apache2 -y -q
 
-# Clone the git repository for HomePiSecurity
-sudo mkdir /apps
-cd /apps
-sudo git clone https://github.com/darinbunker/HomePiSecurity.git
-
 # Install the HomePiSecurity Database
-mysql -u root -pTheMySQLPass1 < /apps/HomePiSecurity/database/home_security_db_script.sql
+mysql -u root -p$MySQLPass < /apps/HomePiSecurity/database/home_security_db_script.sql
+
+# Copy execution file to startup location
+sudo cp /apps/HomePiSecurity/engine/armed-engine.sh /etc/init.d/armed-engine.sh
 
 exit 0
