317fdae9e75ce013ff057367b3c731ddabebf9c2
Darin Bunker <darin.bunker@domo.com>
2015-11-22 22:18:46 -0700

    added support for install of engine

--- engine/armed-engine.sh
+++ engine/armed-engine.sh
@@ -11,7 +11,7 @@
 ### END INIT INFO
  
 # Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=/apps/HomePiSecurity/engine
+DIR=ReplaceHomeDirectory
 DAEMON=$DIR/home_armed.py
 DAEMON_NAME=armed-engine
  
