f016effd505c221df6d04db16ef4bac11c4c43f7
Darin Bunker <darin.bunker@domo.com>
2015-11-29 16:37:13 -0700

    added full path to the script execution items

--- engine/configure_settings.sh
+++ engine/configure_settings.sh
@@ -3,6 +3,8 @@
 # ################################################################ # 
 # update settings file for security engine
 
+installdir=$(cd $(dirname $0); pwd -P)
+
 printf "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #\n"
 printf "# Step 1 -  Obtain Email Settings \n"
 printf "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #\n"
@@ -19,27 +21,27 @@ read email_smtp_port
 # Now check settings
 
 # Update settings file
-sudo sed -i "s:ReplaceSMTPServername:$email_smtp:g" ./configSettings.py
-sudo sed -i "s:ReplaceSMTPServerPort:$email_smtp_port:g" ./configSettings.py
+sudo sed -i "s:ReplaceSMTPServername:$email_smtp:g" $installdir/configSettings.py
+sudo sed -i "s:ReplaceSMTPServerPort:$email_smtp_port:g" $installdir/configSettings.py
 
 printf "\n"
 printf "Please enter email address account (i.e. myemailaddress@gmail.com): \n"
 
 read email_send_acct
-sudo sed -i "s:ReplaceEmailSendAccount:$email_send_acct:g" ./configSettings.py
+sudo sed -i "s:ReplaceEmailSendAccount:$email_send_acct:g" $installdir/configSettings.py
 
 printf "\n"
 printf "Please enter email address account password: \n"
 
 read email_send_acct_pass
-sudo sed -i "s:ReplaceEmailAccountPassword:$email_send_acct_pass:g" ./configSettings.py
+sudo sed -i "s:ReplaceEmailAccountPassword:$email_send_acct_pass:g" $installdir/configSettings.py
 
 
 printf "\n"
 printf "Please enter email address who is sending message (i.e. this is the from account): \n"
 
 read email_from_acct
-sudo sed -i "s:ReplaceEmailFromAccount:$email_from_acct:g" ./configSettings.py
+sudo sed -i "s:ReplaceEmailFromAccount:$email_from_acct:g" $installdir/configSettings.py
 
 
 printf "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #\n"
@@ -51,20 +53,20 @@ printf "Please enter Account SID (i.e. Af8d2dcc20eCf18e231b85ffbac6fee3a6: \n"
 read sms_acct
 
 # Update settings file
-sudo sed -i "s:ReplaceSMSAccountID:$sms_acct:g" ./configSettings.py
+sudo sed -i "s:ReplaceSMSAccountID:$sms_acct:g" $installdir/configSettings.py
 
 printf "\n"
 printf "Please enter SMS Authentication Token (i.e. 82b546e6fdaab955b5bdaa53b37d84b9): \n"
 
 read sms_auth_token
-sudo sed -i "s:ReplaceSMSAuthToken:$sms_auth_token:g" ./configSettings.py
+sudo sed -i "s:ReplaceSMSAuthToken:$sms_auth_token:g" $installdir/configSettings.py
 
 
 printf "\n"
 printf "Please enter SMS phone number - must start with '+' (i.e. +18015551234): \n"
 
 read sms_number
-sudo sed -i "s:ReplaceSMSNumber:$sms_number:g" ./configSettings.py
+sudo sed -i "s:ReplaceSMSNumber:$sms_number:g" $installdir/configSettings.py
 
 printf "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #\n"
 printf "# Setting Update Complete \n"
