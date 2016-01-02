cb7a4b7b676ab6f0e435d4455ac0bb0bb17e78bb
Darin Bunker <darin.bunker@domo.com>
2016-01-02 16:55:25 -0700

    set default configuration path for web service

--- web/www/js/config.js
+++ web/www/js/config.js
@@ -1,4 +1,2 @@
-
-//var url = "https://SetServicePathVariable:SetServicePortVariable";
-var url = "https://localhost:8443";
-
+var url = "https://SetServicePathVariable:SetServicePortVariable";
+//var url = "https://localhost:8443"; - Used for debugging
